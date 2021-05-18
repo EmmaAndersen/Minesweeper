4349adf2bdceef3c603391805ed49aef278d5fac
EmmaAndersen <48715807+EmmaAndersen@users.noreply.github.com>
2021-05-18 19:27:52 +0200

    New timer logic with Chronometer. Not finished yet
    Found this Android thing Chronometer and found it much more useful and easy to implement instead of creating a timer from scratch.

--- app/src/main/java/com/example/minesweeper/Timer.java
+++ app/src/main/java/com/example/minesweeper/Timer.java
@@ -6,8 +6,11 @@ package com.example.minesweeper;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -17,24 +20,26 @@ import java.util.Locale;
 
 public class Timer extends AppCompatActivity {
 
-    private TextView displayedTimer;
+    /*private TextView displayedTimer;
     private Button pauseButton;
-    private Button resetButton;
+    private Button resetButton;*/
 
     boolean timerIsRunning;
-    private int minutes;
+    private Chronometer chronometer;
+    private long pauseOffset;  //To calculate the time difference from when we started the chronometer and until it is paused.
+   /* private int minutes;
     private int seconds;
     private long milliseconds;
-    private String totalTime;
+    private String totalTime;*/
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Init Buttons and the timer TextView with R.id.buttonID
+        //Init Buttons and the chronometer TextView with R.id.ID
 
-        pauseButton.setOnClickListener(new View.OnClickListener() {
+        /*pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (timerIsRunning) {
@@ -52,27 +57,35 @@ public class Timer extends AppCompatActivity {
             }
         });
 
-        UpdateTimer();
+        UpdateTimer();*/
     }
 
-    private void StartTimer() {
-        timerIsRunning = true;
+    public void StartTimer(View view) {
+        if(!timerIsRunning){
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);  //Return {@link Clock} that starts at system boot, including time spent in sleep. starts at 0 and subtracts the offset.
+            chronometer.start();
+            timerIsRunning = true;
+        }
     }
 
-    private void PauseTimer() {
+    public void PauseTimer(View view) {
+        if(timerIsRunning){
+            chronometer.stop();  //this only stops the text, the chronometer still ticks with only this
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();  //This actually pauses the timer.
+            timerIsRunning = false;
+        }
+    }
+
+    public void ResetTimer(View view) {
 
     }
 
-    private void ResetTimer() {
-
-    }
-
-    private void UpdateTimer() {
+   /* private void UpdateTimer() {
         minutes = (int) milliseconds / 1000 / 60;
         seconds = (int) milliseconds / 1000 % 60;
 
         totalTime = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
 
         displayedTimer.setText(totalTime);
-    }
+    }*/
 }
