22abccc4d57ac2782309c182865cb61070e4fbf4
EmmaAndersen <48715807+EmmaAndersen@users.noreply.github.com>
2021-05-18 19:50:18 +0200

    Finished Chronometer timer
    Works fine!
    Has a start, pause and reset function to be handled by buttonclicks.

--- app/src/main/java/com/example/minesweeper/Timer.java
+++ app/src/main/java/com/example/minesweeper/Timer.java
@@ -5,62 +5,29 @@ package com.example.minesweeper;
  * */
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.os.SystemClock;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Chronometer;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Locale;
-
 public class Timer extends AppCompatActivity {
-
-    /*private TextView displayedTimer;
-    private Button pauseButton;
-    private Button resetButton;*/
 
     boolean timerIsRunning;
     private Chronometer chronometer;
     private long pauseOffset;  //To calculate the time difference from when we started the chronometer and until it is paused.
-   /* private int minutes;
-    private int seconds;
-    private long milliseconds;
-    private String totalTime;*/
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Init Buttons and the chronometer TextView with R.id.ID
-
-        /*pauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (timerIsRunning) {
-                    PauseTimer();
-                } else {
-                    StartTimer();
-                }
-            }
-        });
-
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ResetTimer();
-            }
-        });
-
-        UpdateTimer();*/
+        chronometer = findViewById(R.id.chronometerID);
     }
 
-    public void StartTimer(View view) {
+    //Call the following methods with the co-responding buttons' onClick methods.
+
+    public void StartTimer() {
         if(!timerIsRunning){
             chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);  //Return {@link Clock} that starts at system boot, including time spent in sleep. starts at 0 and subtracts the offset.
             chronometer.start();
@@ -68,7 +35,7 @@ public class Timer extends AppCompatActivity {
         }
     }
 
-    public void PauseTimer(View view) {
+    public void PauseTimer() {
         if(timerIsRunning){
             chronometer.stop();  //this only stops the text, the chronometer still ticks with only this
             pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();  //This actually pauses the timer.
@@ -76,16 +43,8 @@ public class Timer extends AppCompatActivity {
         }
     }
 
-    public void ResetTimer(View view) {
-
+    public void ResetTimer() {
+        chronometer.setBase(SystemClock.elapsedRealtime()); //Sets back to 0.
+        pauseOffset = 0;
     }
-
-   /* private void UpdateTimer() {
-        minutes = (int) milliseconds / 1000 / 60;
-        seconds = (int) milliseconds / 1000 % 60;
-
-        totalTime = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-
-        displayedTimer.setText(totalTime);
-    }*/
 }
