5112b3afdc017c99c25c27dfb6774d008a0073a1
EmmaAndersen <48715807+EmmaAndersen@users.noreply.github.com>
2021-05-19 11:37:24 +0200

    UX design for highscores done
    Created with a ViewPagerAdapter with tabs for each difficulty

---
+++ app/src/main/java/com/example/minesweeper/Easy_fragment.java
@@ -0,0 +1,25 @@
+package com.example.minesweeper;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Easy_fragment extends Fragment {
+
+    private View view;
+
+    public Easy_fragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.easy_fragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/minesweeper/HighScoreActivity.java
@@ -0,0 +1,37 @@
+package com.example.minesweeper;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
+public class HighScoreActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highscore);
+        tabLayout = (TabLayout) findViewById(R.id.tablayoutID);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appBarID);
+        viewPager = (ViewPager) findViewById(R.id.viewpageID);
+        ViewAdapter adapter = new ViewAdapter(getSupportFragmentManager());
+
+        // Add fragments
+        adapter.AddAFragment(new Easy_fragment(), "Easy");
+        adapter.AddAFragment(new Intermediate_fragment(), "Intermediate");
+        adapter.AddAFragment(new Hard_fragment(), "hard");
+
+        //Adapter setup
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
